570cc42c50ff7190b57d9a56aa3dcd68824088a0
Robert Ebafua <robertebafua@gmail.com>
2024-06-17 13:54:18 +0100

    part04-Part04_27.IsItInTheFile

---
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -0,0 +1,36 @@
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class IsItInTheFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        System.out.println("Search for:");
+        String searchedFor = scanner.nextLine();
+
+        boolean found = false;
+        try (Scanner scanner1 = new Scanner(Paths.get(file))) {
+            while (scanner1.hasNextLine()) {
+                String line = scanner1.nextLine();
+                if (line.equals(searchedFor)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+
+
+    }
+}
