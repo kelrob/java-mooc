559ab74b706e3f98c2f2d87b532023fe739a3428
Robert Ebafua <robertebafua@gmail.com>
2024-06-18 14:24:49 +0100

    part05-Part05_15.Archive

---
+++ part05-Part05_15.Archive/src/main/java/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    private final String identifier;
+    private final String name;
+
+    public Item(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (!(compared instanceof Item)) {
+            return false;
+        }
+        Item comparedItem = (Item) compared;
+        return this.identifier.equals(comparedItem.identifier);
+    }
+
+}
