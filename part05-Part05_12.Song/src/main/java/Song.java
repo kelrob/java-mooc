0f2ae4592e9aebe17b5bd82f991fbcd434952006
Robert Ebafua <robertebafua@gmail.com>
2024-06-18 14:23:56 +0100

    part05-Part05_12.Song

---
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -0,0 +1,34 @@
+public class Song {
+
+    private final String artist;
+    private final String name;
+    private final int durationInSeconds;
+
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+
+        Song comparedSong = (Song) compared;
+
+        return this.artist.equals(comparedSong.artist) && this.name.equals(comparedSong.name) && this.durationInSeconds
+                == comparedSong.durationInSeconds;
+    }
+
+}
