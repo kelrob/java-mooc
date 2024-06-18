5c41badc164fb26625dc07c048f8caf5bd8bc0ca
Robert Ebafua <robertebafua@gmail.com>
2024-06-18 14:24:13 +0100

    part05-Part05_13.IdenticalTwins

---
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    private final String name;
+    private final SimpleDate birthday;
+    private final int height;
+    private final int weight;
+
+    public Person(String name, SimpleDate birthday, int height, int weight) {
+        this.name = name;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person comparedPerson = (Person) compared;
+
+        return this.name.equals(comparedPerson.name) && this.birthday.equals(comparedPerson.birthday) && this.height == comparedPerson.height && this.weight == comparedPerson.weight;
+    }
+}
